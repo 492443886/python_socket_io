18305902495305cec62add4a6c6a0d72094a5b9f
492443886 <machunhui492443886@gmail.com>
2024-11-07 00:15:06 -0500

    add thing

--- pi_client.py
+++ pi_client.py
@@ -1,6 +1,16 @@
 import socketio
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
 
 sio = socketio.Client()
+
+def set_light_on_off(pca, channel, state):
+    if state == "on":
+        pca._pca.channels[channel].duty_cycle = 0xFFFF  # Full duty cycle (Light ON)
+    elif state == "off":
+        pca._pca.channels[channel].duty_cycle = 0x0000  # 0 duty cycle (Light OFF)
+    else:
+        print("Invalid state. Use 'on' or 'off'.")
 
 
 @sio.event
@@ -29,13 +39,15 @@ def process(data):
 
     if "info" in data:
         print("info:", data["info"])
+        set_light_on_off(kit, 2, "on")  # Light ON
     else:
         print("'info' key does not exist in data")
+        set_light_on_off(kit, 2, "off")  # Light ON
 
 
 if __name__ == '__main__':
     # Connect to the server
-    sio.connect("http://localhost:12345")   
+    sio.connect("http://10.0.0.199:9001")   
     while True:
         pass
 
